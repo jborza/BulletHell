bbc2feece2da2021bf60d794e623f46c7451ee8b
Juraj Borza <jborza@gmail.com>
2024-03-09 20:40:42 +0100

    extracted player movement to a function

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 var speed : int = 75
 var direction : Vector2 = Vector2(0,1)
 
+func setup_frame_and_fliph(frame: float, flip_h: bool):
+	get_node("Player").frame = frame
+	get_node("Player").flip_h = flip_h
+
 func _physics_process(delta: float) -> void:
 	var inputDir : Vector2 = Vector2(
 		Input.get_axis("ui_left", "ui_right"),
@@ -11,21 +15,17 @@ func _physics_process(delta: float) -> void:
 	
 	if inputDir.x > 0:
 		#check if the player is moving right
-		get_node("Player").frame = 1
-		get_node("Player").flip_h = false
+		setup_frame_and_fliph(1, false)
 		direction = inputDir
 	elif inputDir.x < 0:
 		direction = inputDir
-		get_node("Player").frame = 1
-		get_node("Player").flip_h = true
+		setup_frame_and_fliph(1, true)
 	elif inputDir.y < 0:
 		direction = inputDir
-		get_node("Player").frame = 2
-		get_node("Player").flip_h = false
+		setup_frame_and_fliph(2, false)
 	elif inputDir.y > 0:
 		direction = inputDir
-		get_node("Player").frame = 0
-		get_node("Player").flip_h = false
+		setup_frame_and_fliph(0, false)
 		
 	velocity = inputDir * speed
 	move_and_slide()
