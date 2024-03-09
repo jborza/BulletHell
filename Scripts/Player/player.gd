68d4bbdb2a3c45032f653ae94286fa577a3c13da
Juraj Borza <jborza@gmail.com>
2024-03-09 20:38:03 +0100

    moving to 4 directions

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -11,13 +11,21 @@ func _physics_process(delta: float) -> void:
 	
 	if inputDir.x > 0:
 		#check if the player is moving right
+		get_node("Player").frame = 1
+		get_node("Player").flip_h = false
 		direction = inputDir
 	elif inputDir.x < 0:
 		direction = inputDir
+		get_node("Player").frame = 1
+		get_node("Player").flip_h = true
 	elif inputDir.y < 0:
 		direction = inputDir
+		get_node("Player").frame = 2
+		get_node("Player").flip_h = false
 	elif inputDir.y > 0:
 		direction = inputDir
+		get_node("Player").frame = 0
+		get_node("Player").flip_h = false
 		
 	velocity = inputDir * speed
 	move_and_slide()
